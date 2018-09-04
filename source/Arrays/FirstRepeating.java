df2d4adff299444b8060ef47ae34584b46d1f4fa
Madhu <madhu.saragadam@kony.com>
2018-09-04 08:44:15 +0530

    Printing the result

---
+++ source/Arrays/FirstRepeating.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.*;
+
+public class FirstRepeating {
+
+	public static void main(String args[]) {
+		
+		Scanner input = new Scanner(System.in);
+		int size = input.nextInt();
+		
+		
+	}
+}
